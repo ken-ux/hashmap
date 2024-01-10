d437f7ed0f16bc3c8064ae83a2cdedee875753bb
Kenny Nguyen <sokenns@gmail.com>
2024-01-10 03:17:29 -0800

    Uses objects in buckets array instead of single values

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-if (index < 0 || index >= buckets.length) {
-  throw new Error("Trying to access index out of bound");
-}
+// if (index < 0 || index >= buckets.length) {
+//   throw new Error("Trying to access index out of bound");
+// }
 
 class HashMap {
   constructor(size) {
@@ -21,15 +21,25 @@ class HashMap {
   set(key, value) {
     // Reduces large hash codes into smaller integers
     let bucket = this.hash(key) % this.buckets.length;
-    this.buckets[bucket] = value;
+
+    this.buckets[bucket] = { [key]: value };
   }
 
   get(key) {
     // Reduces large hash codes into smaller integers
     let bucket = this.hash(key) % this.buckets.length;
-    
+
+    // Check if bucket exists
     if (this.buckets[bucket]) {
-      return this.buckets[bucket];
+      let bucketKey = Object.keys(this.buckets[bucket])[0];
+
+      // Check if key already in the bucket matches the key that was given
+      if (key === bucketKey) {
+        // Return key's value
+        return this.buckets[bucket][bucketKey];
+      } else {
+        throw new Error("Key does not match.");
+      }
     } else {
       return null;
     }
@@ -49,3 +59,7 @@ class HashMap {
 
   entries() {}
 }
+
+let test = new HashMap(16);
+test.set("kermit", 5);
+console.log(test.get("piy"));
